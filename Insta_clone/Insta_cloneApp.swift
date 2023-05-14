455d789d7d43256343d361f0df3ce4a8ef72bc7b
Mahendra raut <mahendra.raut@payiq.net>
2023-05-14 11:55:14 +0530

    Initial Commit

---
+++ Insta_clone/Insta_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Insta_cloneApp.swift
+//  Insta_clone
+//
+//  Created by Nayan Khadase on 14/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Insta_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
